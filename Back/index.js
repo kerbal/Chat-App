432961c9bb641fa6f679c482b2b5707fc209af71
kerbal <tranquockhanh997@gmail.com>
2019-12-30 11:56:10 +0700

    init backend

---
+++ Back/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import io from 'socket.io';
+import http from 'http';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send('running');
+});
+
+const server = http.Server(app);
+const socket = io(server);
+
+socket.on('connection', (socket) => {
+  console.log('a user connect');
+  
+  socket.on('disconnect', () => {
+    console.log('a user disconnect');
+  })
+});
+
+const port = process.env.PORT || 3001;
+server.listen(port, async () => {
+  console.log(`Server is up on port ${port}`);
+});
